9741b001bbdf4df6d7cfb69f4cf33ec52b139522
paula.silveira <paulapradosilveira@hotmail.com>
2012-10-18 11:08:34 -0300

    Teste

---
+++ Controle_de_Producao_Roupas/bd/bd_fabrica.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE materiaPrima(
+	codMateriaPrima SERIAL PRIMARY KEY,
+	nome VARCHAR(40),
+	valor MONEY,
+	unidade VARCHAR(10),
+	quantidade INTEGER,
+	quantMin INTEGER
+);
+
+CREATE TABLE produtoPronto(
+	codProdutoPronto SERIAL PRIMARY KEY,
+	nome VARCHAR(40),
+	valor MONEY,
+	quantidade INTEGER,
+	quantMin INTEGER
+);
+
+CREATE TABLE materiaProduto(
+	codProdutoPronto INTEGER REFERENCES produtoPronto,
+	codMateriaPrima INTEGER REFERENCES materiaPrima,
+	quantidade INTEGER,
+	PRIMARY KEY (codProdutoPronto, codMateriaPrima)
+);

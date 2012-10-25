24bcfc2d48e171c0901c5cc49d69e59c1bd9d59d
paula.silveira <paulapradosilveira@hotmail.com>
2012-10-25 10:48:35 -0200

    novo database

---
+++ Controle_de_Producao_Roupas/src/senac/lp2/producao/database/Connect.java
@@ -0,0 +1,24 @@
+package senac.lp2.producao.database;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Connect {
+//	public void connectToAndQueryDatabase(String username, String password) {
+//
+//	    Connection con = DriverManager.getConnection(
+//	                         "jdbc:myDriver:myDatabase",
+//	                         username,
+//	                         password);
+//
+//	    Statement stmt = con.createStatement();
+//	    ResultSet rs = stmt.executeQuery("SELECT a, b, c FROM Table1");
+//
+//	    while (rs.next()) {
+//	        int x = rs.getInt("a");
+//	        String s = rs.getString("b");
+//	        float f = rs.getFloat("c");
+//	    }
+//	}
+}
